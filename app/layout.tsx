0092cf1c8ddb78bc987321809f80901c7d016a07
franli <fei.li.best@gmail.com>
2023-03-04 09:33:23 +0800

    add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import ThemeProvider from '@utils/Theme'
 import Toast from '@utils/Toaster'
+import { AnalyticsWrapper } from '@utils/analytics';
 
 export default function RootLayout({children}:{children: React.ReactNode}) {
   
@@ -8,10 +9,9 @@ export default function RootLayout({children}:{children: React.ReactNode}) {
     <html lang="en" data-theme="bumblebee">
       <head/>
         <ThemeProvider>
-          <>
           <Toast/>
           {children}
-          </>
+          <AnalyticsWrapper />
         </ThemeProvider>
     </html>
   )
