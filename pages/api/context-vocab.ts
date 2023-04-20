27c97c0256e65f65c0ea1e74b161d19158953d3a
franli <fei.li.best@gmail.com>
2023-04-20 11:20:56 +0800

    add vercel edge config for context-vocab.ts

--- pages/api/context-vocab.ts
+++ pages/api/context-vocab.ts
@@ -1,5 +1,9 @@
 import { Configuration, OpenAIApi } from "openai";
 
+export const config = {
+  runtime: "edge",
+};
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
