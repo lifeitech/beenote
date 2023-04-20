9035336a16b1939ecd2ad303582fa37f84f6b97a
franli <fei.li.best@gmail.com>
2023-04-20 11:34:38 +0800

    remove vercel edge function

--- pages/api/context-vocab.ts
+++ pages/api/context-vocab.ts
@@ -1,9 +1,5 @@
 import { Configuration, OpenAIApi } from "openai";
 
-export const config = {
-  runtime: "edge",
-};
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
