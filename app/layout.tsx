e0d0f0cffcdcf43001fc3ba2e0e09f07f6cf2eba
Fei Li <fei.li.best@outlook.com>
2023-08-24 15:41:57 +0800

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,29 @@ import './globals.css'
 import ThemeProvider from '@utils/Theme'
 import Toast from '@utils/Toaster'
 import { AnalyticsWrapper } from '@utils/analytics';
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'BeeNote',
+  description: 'Your online language learning notes',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+  },
+
+}
 
 export default function RootLayout({children}:{children: React.ReactNode}) {
   
   return (
     <html lang="en" data-theme="bumblebee">
-      <head/>
+      <head>
+        <link rel="preload" href="/bg1.jpg" as="image" />
+        <link rel="preload" href="/bg2.jpg" as="image" />
+        <link rel="preload" href="/bg3.jpg" as="image" />
+        <link rel="preload" href="/bg4.jpg" as="image" />
+        <link rel="icon" href="/logo.png" />
+      </head>
         <ThemeProvider>
           <Toast/>
           {children}
@@ -16,5 +33,3 @@ export default function RootLayout({children}:{children: React.ReactNode}) {
     </html>
   )
 }
-
-
